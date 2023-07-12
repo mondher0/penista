34efbe376ae5960e8a6d399a43008c4c92a92a38
mondher04000 <m_mameri@estin.dz>
2023-07-12 13:05:33 +0100

    update

--- src/view/commandes/CommandeCard.jsx
+++ src/view/commandes/CommandeCard.jsx
@@ -1,14 +1,29 @@
-import './CommandeCard.css'
-const CommandeCard = () => {
+/* eslint-disable react/prop-types */
+import "./CommandeCard.css";
+const CommandeCard = ({ price, title, quantity }) => {
+  const quantityArray = [];
+
+  for (const key in quantity) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (quantity.hasOwnProperty(key)) {
+      quantityArray.push({ value: key, quantity: quantity[key] });
+    }
+  }
   return (
     <div className="commande-card">
       <img src="https://picsum.photos/200/300" alt="commande" />
       <div className="commande-card-content">
-        <p>T-Shirt</p>
-        <p>30000DA</p>
-        <div>
-          <span>S:</span>
-          <span>1</span>
+        <p>{title}</p>
+        <p>{price}DA</p>
+        <div className="quan">
+          {quantityArray.map((item) => (
+            <>
+              <div className="span">
+                <span>{item.value}:</span>
+                <span>{item.quantity}</span>
+              </div>
+            </>
+          ))}
         </div>
       </div>
     </div>
--- src/view/commandes/SingleCommandePage.jsx
+++ src/view/commandes/SingleCommandePage.jsx
@@ -1,11 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import NavBar from "../shared/navBar/NavBar";
 import { next } from "../../assets/index";
 import { useParams } from "react-router-dom";
 import "./SingleCommandePage.css";
 import CommandeCard from "./CommandeCard";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { baseUrl } from "../../utils/constants";
 
 const SingleCommandePage = () => {
   const { id } = useParams();
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const getSingleCommande = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(`${baseUrl}order/${id}/`);
+      console.log(response);
+      setItems(response.data.data.orderItems);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+  useEffect(() => {
+    getSingleCommande();
+  }, []);
+
   return (
     <>
       <NavBar title="Commandes" />
@@ -15,12 +39,62 @@ const SingleCommandePage = () => {
           <img src={next} />
           <p>{id}</p>
         </div>
-        <div className="commande-cards">
-          <CommandeCard />
-          <CommandeCard />
-          <CommandeCard />
-          <CommandeCard />
-        </div>
+        {isLoading && (
+          <div
+            className="loading"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "70vh",
+            }}
+          >
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: "20px",
+                fontWeight: "bold",
+              }}
+            >
+              Chargement...
+            </p>
+          </div>
+        )}
+
+        {isError && (
+          <div
+            className="loading"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "70vh",
+            }}
+          >
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: "20px",
+                fontWeight: "bold",
+              }}
+            >
+              Something went wrong...
+            </p>
+          </div>
+        )}
+
+        {!isLoading && (
+          <div className="commande-cards">
+            {items?.map((item) => (
+              <CommandeCard
+                key={item.id}
+                title={item.product_name}
+                price={item.price}
+                quantity={item.quantity}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
