27336a85c85539bae0132af9df88475bed1f444a
mondher04000 <m_mameri@estin.dz>
2023-07-17 01:04:20 +0100

    update adding tickets

--- src/view/tickets/TicketsMatcheTable.jsx
+++ src/view/tickets/TicketsMatcheTable.jsx
@@ -1,18 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "../produit/ProductTable.css";
 import { edite } from "../../assets/index";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { baseUrl } from "../../utils/constants";
 
 const TicketsMatcheTable = () => {
+  const [tickets, setTickets] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pages, setPages] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  // get all tickets
+  const getAllTickets = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(
+        `${baseUrl}match/with/ticket/?page=${currentPage}`
+      );
+      console.log(response);
+      if (response.data.data.matchs.length === 0) {
+        setIsEmpty(true);
+      }
+      setTickets(response.data.data.matchs);
+      setPages(response.data.data.pages);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+  // Pagination handlers
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  useEffect(() => {
+    getAllTickets();
+  }, [currentPage]);
+
   return (
     <>
+      {isLoading && <div className="loader">Chargement...</div>}
+      {isError && <div className="loader">Erreur de chargement</div>}
+      {isEmpty && <div className="loader">Aucune Tickets</div>}
       <table className="product-table">
         <thead>
           <tr>
             <th>ID Match</th>
             <th>Date du match</th>
-            <th>Heure du match</th>
             <th>Titre de la ligue</th>
             <th>Equipe adverse</th>
-            <th>Code equipe</th>
             <th>N° Rond</th>
             <th>Stade</th>
             <th>Tiquets</th>
@@ -20,36 +65,34 @@ const TicketsMatcheTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>11-02-2023</td>
-            <td>11:00</td>
-            <td>La liga</td>
-            <td>Leicester</td>
-            <td>LCTR</td>
-            <td>Premier Ligue</td>
-            <td>Maison</td>
-            <td>20</td>
-            <td>
-              <img src={edite} alt="Modifier" />
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>11-02-2023</td>
-            <td>11:00</td>
-            <td>La liga</td>
-            <td>Leicester</td>
-            <td>LCTR</td>
-            <td>Premier Ligue</td>
-            <td>Maison</td>
-            <td>20</td>
-            <td>
-              <img src={edite} alt="Modifier" />
-            </td>
-          </tr>
+          {tickets &&
+            tickets.map((ticket) => (
+              <>
+                <tr>
+                  <td>{ticket.id}</td>
+                  <td>{ticket.startDate}</td>
+                  <td>{ticket.league.name}</td>
+                  <td>{ticket.opposingTeam.name}</td>
+                  <td>{ticket.league.round}</td>
+                  <td>{ticket.is_home ? "Maison" : "Extérieur"}</td>
+                  <td>{ticket.tickets}</td>
+                  <td>
+                    <img src={edite} alt="Modifier" />
+                  </td>
+                </tr>
+              </>
+            ))}
         </tbody>
       </table>
+      <div className="pagination">
+        <button disabled={currentPage === 1} onClick={goToPreviousPage}>
+          Previous
+        </button>
+        <span>Page{currentPage}</span>
+        <button onClick={goToNextPage} disabled={currentPage == pages}>
+          Next
+        </button>
+      </div>
     </>
   );
 };
