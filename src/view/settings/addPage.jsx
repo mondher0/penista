ea6c10731bf4974f697614582bcc0e029ac62e73
mondher04000 <m_mameri@estin.dz>
2023-07-20 11:26:09 +0100

    adding add page in settings

--- src/view/settings/addPage.jsx
+++ src/view/settings/addPage.jsx
@@ -1,16 +1,56 @@
 import NavBar from "../shared/navBar/NavBar";
 import "../produit/AddProductPage.css";
 import "../events/AddEventPage.css";
+import { useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { baseUrl } from "../../utils/constants";
 const AddPage = () => {
+  const [title, setTitle] = useState();
+  const [slogan, setSlogan] = useState();
+  const [description, setDescription] = useState();
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const data = {
+        title: title,
+        slogan: slogan,
+        content: description,
+      };
+      const response = await axiosInstance.post(`${baseUrl}page/create/`, data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <NavBar title="Paramètres" />
       <div className="container">
         <div className="form">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input">
               <label htmlFor="gratuit">Titre</label>
-              <input type="text" id="gratuit" name="gratuit" />
+              <input
+                type="text"
+                id="gratuit"
+                name="gratuit"
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input">
+              <label htmlFor="gratuit">Slogan</label>
+              <input
+                type="text"
+                id="gratuit"
+                name="gratuit"
+                onChange={(e) => {
+                  setSlogan(e.target.value);
+                }}
+              />
             </div>
             <div className="input">
               <label htmlFor="gratuit">Description</label>
@@ -19,6 +59,9 @@ const AddPage = () => {
                 id="description"
                 cols="30"
                 rows="10"
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
               ></textarea>
             </div>
             <button
@@ -28,7 +71,7 @@ const AddPage = () => {
                 marginTop: "0",
               }}
             >
-              Modifier
+              Ajouter
             </button>
           </form>
         </div>
