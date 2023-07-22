95cd624590196d05d88b9f2e1d24f6441a493cb0
mondher04000 <m_mameri@estin.dz>
2023-07-22 20:45:58 +0100

    update

--- src/view/expectations/ExpectationsTable.jsx
+++ src/view/expectations/ExpectationsTable.jsx
@@ -1,6 +1,25 @@
 import "../utilisateurs/DemandeAbonnementTable.css";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { baseUrl } from "../../utils/constants";
 
 const ExpectationsTable = () => {
+  const [expectations, setExpectations] = useState();
+
+  // get all expectations
+  const getAllExpectations = async () => {
+    try {
+      const response = await axiosInstance.get(`${baseUrl}expectation/`);
+      console.log(response);
+      setExpectations(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllExpectations();
+  }, []);
   return (
     <>
       <table className="product-table">
@@ -16,40 +35,44 @@ const ExpectationsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>12-12-2022</td>
-            <td>Liverpool</td>
-            <td>
-              <div className="user-details">
-                <img src="https://picsum.photos/200" alt="user" />
-                <div className="user-info">
-                  <p>Utilisateur 1</p>
-                  <span>mondher@gmail.com</span>
-                </div>
-              </div>
-            </td>
-            <td>Victoire</td>
-            <td>2</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>12-12-2022</td>
-            <td>Liverpool</td>
-            <td>
-              <div className="user-details">
-                <img src="https://picsum.photos/200" alt="user" />
-                <div className="user-info">
-                  <p>Utilisateur 1</p>
-                  <span>mondher@gmail.com</span>
-                </div>
-              </div>
-            </td>
-            <td>Victoire</td>
-            <td>2</td>
-            <td>0</td>
-          </tr>
+          {expectations &&
+            expectations.map((expectation) => {
+              return (
+                <>
+                  <tr key={expectation.id}>
+                    <td>{expectation.id}</td>
+                    <td>{expectation.match_info.startDate}</td>
+                    <td>{expectation.match_info.opposingTeam.name}</td>
+                    <td>
+                      <div className="user-details">
+                        <img
+                          src={`${baseUrl}${expectation.client_info.image}`}
+                          alt="user"
+                        />
+                        <div className="user-info">
+                          <p>
+                            {expectation.client_info.first_name}{" "}
+                            {expectation.client_info.last_name}
+                          </p>
+                          <span>{expectation.client_info.email}</span>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      {expectation.answer.ourTeamGoals >
+                      expectation.answer.opposingTeam
+                        ? "Victoire"
+                        : expectation.answer.ourTeamGoals <
+                          expectation.answer.opposingTeam
+                        ? "Défaite"
+                        : "Nul"}
+                    </td>
+                    <td>{expectation.answer.ourTeamGoals}</td>
+                    <td>{expectation.answer.opposingTeam}</td>
+                  </tr>
+                </>
+              );
+            })}
         </tbody>
       </table>
     </>
