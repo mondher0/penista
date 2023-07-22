a5612b00b384e5bd2a3f8e150665adad5799eada
mondher04000 <m_mameri@estin.dz>
2023-07-22 20:52:12 +0100

    adding paginations

--- src/view/expectations/ExpectationsTable.jsx
+++ src/view/expectations/ExpectationsTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "../utilisateurs/DemandeAbonnementTable.css";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
@@ -5,23 +6,50 @@ import { baseUrl } from "../../utils/constants";
 
 const ExpectationsTable = () => {
   const [expectations, setExpectations] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pages, setPages] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   // get all expectations
   const getAllExpectations = async () => {
     try {
-      const response = await axiosInstance.get(`${baseUrl}expectation/`);
+      setIsLoading(true);
+      const response = await axiosInstance.get(
+        `${baseUrl}expectation/?page=${currentPage}`
+      );
       console.log(response);
       setExpectations(response.data.data);
+      if (response.data.data.length === 0) {
+        setIsEmpty(true);
+      }
+      setPages(response.data.pages);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
+      setIsError(true);
     }
   };
 
   useEffect(() => {
     getAllExpectations();
-  }, []);
+  }, [currentPage]);
+
+  // Pagination handlers
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
   return (
     <>
+      {isLoading && <div className="loader">Chargement...</div>}
+      {isError && <div className="loader">Erreur de chargement</div>}
+      {isEmpty && <div className="loader">Aucune expectation</div>}
       <table className="product-table">
         <thead>
           <tr>
@@ -75,6 +103,15 @@ const ExpectationsTable = () => {
             })}
         </tbody>
       </table>
+      <div className="pagination">
+        <button disabled={currentPage === 1} onClick={goToPreviousPage}>
+          Previous
+        </button>
+        <span>Page{currentPage}</span>
+        <button onClick={goToNextPage} disabled={currentPage == pages}>
+          Next
+        </button>
+      </div>
     </>
   );
 };
