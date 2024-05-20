c49d2f4d0960710b760df5f7f594060c291158c2
mondher <110062316+mondher0@users.noreply.github.com>
2024-05-20 22:52:42 +0100

    adding another type of event and build

--- src/view/events/AddEventPage.jsx
+++ src/view/events/AddEventPage.jsx
@@ -176,6 +176,7 @@ const AddEventPage = () => {
                 <option value="Regroupement">Regroupement</option>
                 <option value="voyage">voyage</option>
                 <option value="Compétition">Compétition</option>
+                <option value="fane zone">Fan Zone</option>
               </select>
             </div>
             <div className="input desc">
